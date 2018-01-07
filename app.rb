fd562723bbce47761e656040795cb2acb5e6ed89
Hoon Lee <yl.hoony@gmail.com>
2018-01-07 09:00:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
-    session["foo"] == 'hello'
+    session[:foo] == 'hello'
     puts session
 
     if session[:foo] == 'hello'
